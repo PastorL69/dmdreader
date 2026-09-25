7f35d963da2d55d6e3e09917c9bbc8924f463006
Jan Vos <jjcvos@hotmail.nl>
2026-09-25 20:42:07 +0200

    simple debug

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -272,10 +272,12 @@ bool spi_send_pix(uint8_t *pixbuf, bool skip_when_busy) {
 
   //spi_send_blocking((uint32_t *)&h, sizeof(h));
   pio_sm_put_blocking(spi_pio, spi_sm, 1001);
+  uint32_t word = pio_sm_get_blocking(spi_pio, spi_sm);
+  Serial.printf("word: ", word);
   //spi_send_blocking((uint32_t *)&ph, sizeof(ph));
-  spi_send_dma((uint32_t *)pixbuf, target_bytes);
-  
-  dma_channel_wait_for_finish_blocking(spi_dma_channel);
+  //spi_send_dma((uint32_t *)pixbuf, target_bytes);
+
+  //dma_channel_wait_for_finish_blocking(spi_dma_channel);
   start_spi();
 
   return true;
