33d7c514359370cdc2e8dc5e8d003df64fef5e7b
Jan Vos <jjcvos@hotmail.nl>
2026-09-25 03:09:40 +0200

    test single debug

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -171,11 +171,11 @@ void spi_send_dma(uint32_t *buf, uint16_t len) {
  * @param buf a byte buffer
  * @param len
  */
-static inline __attribute__((always_inline)) void spi_send_blocking(
-    uint32_t *buf, uint16_t len) {
+void spi_send_blocking(uint32_t *buf, uint16_t len) {
   for (uint16_t i = 0; i < len; i += 4) {
     pio_sm_put_blocking(spi_pio, spi_sm, *buf);
     buf++;
+    Serial.printf("debug");
   }
 }
 
