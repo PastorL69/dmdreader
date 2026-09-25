9c4d37268404689fcb46b8523739d7277e75ddc9
Jan Vos <jjcvos@hotmail.nl>
2026-09-25 22:07:07 +0200

    try no inl

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -243,7 +243,7 @@ void spi_clean_exit() {
  * @param buf a byte buffer
  * @param len
  */
-void spi_send_blocking(volatile uint32_t *buf, uint16_t len) {
+void __attribute__((noinline)) spi_send_blocking(uint32_t *buf, uint16_t len) {
   for (uint16_t i = 0; i < len; i += 4) {
     pio_sm_put_blocking(spi_pio, spi_sm, *buf);
     buf++;
