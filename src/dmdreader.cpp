fd44200af2c02a7c26fe106a5e0a06e8e02d9a3a
Jan Vos <jjcvos@hotmail.nl>
2026-09-25 20:38:24 +0200

    try this test

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -270,8 +270,9 @@ bool spi_send_pix(uint8_t *pixbuf, bool skip_when_busy) {
     if (spi_busy()) return false;
   }
 
-  spi_send_blocking((uint32_t *)&h, sizeof(h));
-  spi_send_blocking((uint32_t *)&ph, sizeof(ph));
+  //spi_send_blocking((uint32_t *)&h, sizeof(h));
+  pio_sm_put_blocking(spi_pio, spi_sm, 1001);
+  //spi_send_blocking((uint32_t *)&ph, sizeof(ph));
   spi_send_dma((uint32_t *)pixbuf, target_bytes);
   dma_channel_wait_for_finish_blocking(spi_dma_channel);
   start_spi();
