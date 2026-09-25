087b88f102b0238c862c4aa69211d2bd9951edf1
Jan Vos <jjcvos@hotmail.nl>
2026-09-25 22:19:09 +0200

    try iyt

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -244,6 +244,8 @@ void spi_clean_exit() {
  * @param len
  */
 void __attribute__((noinline)) spi_send_blocking(uint32_t *buf, uint16_t len) {
+
+  __asm__ __volatile__("" ::: "memory");
   for (uint16_t i = 0; i < len; i += 4) {
     pio_sm_put_blocking(spi_pio, spi_sm, *buf);
     buf++;
